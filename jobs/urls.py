87908c5b9399239cdfc4a4dbb7d87757b10b3185
Nouman Mahmood <noumanmhd@outlook.com>
2023-08-12 19:08:47 +0300

    Fixing issues

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,15 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('job_descriptions/', views.job_description_list,
-         name='job_description_list'),
-    path('job_descriptions/<int:pk>/', views.job_description_detail,
+    path('job/', views.job_description_list,  name='job_description_list'),
+    path('job_descriptions/<uuid:pk>/', views.job_description_detail,
          name='job_description_detail'),
-    path('job_descriptions/new/', views.job_description_create,
+    path('job/new/', views.job_description_create,
          name='job_description_create'),
-    path('job_descriptions/<int:pk>/edit/',
-         views.job_description_edit, name='job_description_edit'),
+    path('job/<uuid:pk>/edit/',  views.job_description_edit,
+         name='job_description_edit'),
     path('', views.home, name='index'),
-    path('job_descriptions/<int:pk>/delete/',
+    path('job/<uuid:pk>/delete/',
          views.job_description_delete, name='job_description_delete'),
 ]
